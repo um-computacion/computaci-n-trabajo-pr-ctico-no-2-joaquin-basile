034401ff58d191495829a14c4c60ed88bbdc5b69
joaquin-basile <j.basile@alumno.um.edu.ar>
2025-04-14 18:38:22 -0300

    Se implemento clean_text
    Se implemento la funcion clean_text que limpia el input del usuario de
    todos los signos de puntuacion o espacios que contenga el mismo, se
    utilizo la libreria standar string de python para listar los signos y se
    añadio los que faltaban.
    Ahora el codigo pasa todos los test exitosamente

--- src/palindrome.py
+++ src/palindrome.py
@@ -1,5 +1,15 @@
+import string
+def clean_text(phrase: str)-> str:
+    marks = string.punctuation + "¿¡ "
+    cleaned = ''
+    for word in phrase.lower():
+        if not word in marks:
+            cleaned += word
+    return cleaned
+
 def is_palindrome(userPhrase: str)-> bool:
-    return userPhrase == userPhrase[::-1] 
+    phraseCleaned = clean_text(userPhrase)
+    return phraseCleaned == phraseCleaned[::-1] 
 
 
 if __name__ == '__main__':
